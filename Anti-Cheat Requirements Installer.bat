83511ab9ff04f9fb2c49d9866a13fcdde0e100aa
SH4FS0c13ty <52138924+SH4FS0c13ty@users.noreply.github.com>
2019-07-30 12:21:16 +0200

    Add files via upload

--- Anti-Cheat Requirements Installer.bat
+++ Anti-Cheat Requirements Installer.bat
@@ -76,11 +76,7 @@ goto end
 
 :basic_install
 echo Installation des dépendances Python ...
-IF EXIST "C:\Program Files (x86)\Python37-32" (
-	"C:\Program Files (x86)\Python37-32\python.exe" -m pip install -r "%dir%\setup\requirements.txt"
-) ELSE (
-	"C:\Program Files\Python37-32\python.exe" -m pip install -r "%dir%\setup\requirements.txt"
-)
+python -m pip install -r "%dir%\setup\requirements.txt"
 echo Téléchargement de Tesseract 5 x86 ...
 "%dir%\setup\curl.exe" -# -k -o "%dir%\setup\tesseract_v5_x86.exe" https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w32-setup-v5.0.0-alpha.20190708.exe
 echo Installation de Tesseract ...
